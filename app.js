b64556c449e5fee1309ff4fc35e8ee6132cb5474
hanna <hanna8413@naver.com>
2023-07-15 16:43:21 +0900

    intro2

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // INTRO
 
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    introFlower.style.display = 'block';
+  }, 1500);
+
+  setTimeout(() => {
+    introFlower.style.left = `-${introFlower.offsetWidth}px`;
+    introFlower.style.width = '30%';
+    introLogo.remove();
+  }, 4000);
+
+  setTimeout(() => {
+    intro.remove();
+  }, 4500);
+});
