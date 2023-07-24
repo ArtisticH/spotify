6cc6031e0bd003e3f366300d678c7917333bf5df
hanna <hanna8413@naver.com>
2023-07-24 15:49:31 +0900

    spotp11

--- app.js
+++ app.js
@@ -74,28 +74,62 @@ const $spotImgRead = document.querySelectorAll('.spotImgRead');
 const $spotFlex = document.querySelector('.spotFlex');
 
 $spotImg.forEach((img) => {
-  
+
   img.addEventListener('mouseenter', (e) => {
     let index = [...$spotImg].indexOf(img);
     [...$spotImgRead].forEach((item) => {
       item.classList.remove('hover');
     });
     if(e.target === $spotImg[index] && (e.relatedTarget === $spotFlex || e.relatedTarget === e.target.parentNode)) {
+      // spotSvg.classList.add('hover');
       $spotImgRead[index].classList.add('hover');
-      console.log('enter', $spotImg[index], $spotImgRead[index], e.target, e.relatedTarget);
     }
   });
 
 
   img.addEventListener('mouseleave', (e) => {
-    console.log('leavelrelated', e.relatedTarget);
     let index = [...$spotImg].indexOf(img);
-    if(e.target === $spotImg[index] && (e.relatedTarget === $spotFlex || e.relatedTarget === e.target.parentNode)) {
+    if((e.target === $spotImg[index]) && (e.relatedTarget === $spotFlex || e.relatedTarget === e.target.parentNode)) {
+      // spotSvg.classList.remove('hover');
       $spotImgRead[index].classList.remove('hover');
-      console.log('leave', $spotImg[index], $spotImgRead[index],  e.target, e.relatedTarget);
     }
   });
 });
+
+// ----------------------------------------------------------------------------------
+// SEC 3 - INBOX
+
+(function() {
+  const $content = document.getElementById('sec4');
+  const $path1Svg1 = document.querySelector('.path1_svg1');
+  const $path2Svg1 = document.querySelector('.path2_svg1');
+  const $path1Svg1Length = $path1Svg1.getTotalLength();
+  const $path2Svg1Length = $path2Svg1.getTotalLength();
+  const $path1Svg2 = document.querySelector('.path1_svg2');
+  const $path2Svg2 = document.querySelector('.path2_svg2');
+  const $path1Svg2Length = $path1Svg2.getTotalLength();
+  const $path2Svg2Length = $path2Svg2.getTotalLength();
+  console.log($path1Svg1Length, $path2Svg1Length);
+  console.log($path1Svg2Length, $path2Svg2Length);
+
+  window.addEventListener('scroll', () => {
+    const ratio = (window.scrollY + window.innerHeight - $content.offsetTop) / $content.offsetHeight;
+    console.log(ratio);
+
+    if (ratio >= 0.4 && ratio < 1.75) {
+      $path1Svg1.classList.add('active');
+      $path2Svg1.classList.add('active');
+      $path1Svg2.classList.add('active');
+      $path2Svg2.classList.add('active');
+    } else if (ratio <= 0 || ratio >= 1.77) {
+      $path1Svg1.classList.remove('active');
+      $path2Svg1.classList.remove('active');
+      $path1Svg2.classList.remove('active');
+      $path2Svg2.classList.remove('active');
+    }
+  })
+})();
+
 
 
 // ----------------------------------------------------------------------------------
@@ -115,6 +149,61 @@ jobs_rols.addEventListener('mouseleave', () => {
   miami_arrow.classList.toggle('hover');
   jobs_editorial.classList.toggle('hover');
 });
+
+// ----------------------------------------------------------------------------------
+// TIME
+
+// const $se = document.getElementById('s8_se').querySelector('.s8_time');
+// const $uk = document.getElementById('s8_uk').querySelector('.s8_time');
+// const $us = document.getElementById('s8_us').querySelector('.s8_time');
+
+// function showLondonTime() {
+//   const londonTime = new Date().toLocaleString('en-GB', {
+//     timeZone: 'Europe/London',
+//     hour: 'numeric',
+//     minute: 'numeric',
+//     second: 'numeric',
+//     hour12: false
+//   });
+
+//   const formattedTime = londonTime.replace(/:/g, ' : ');
+//   $uk.textContent = formattedTime;
+// }
+
+// function showSeTime() {
+//   const seTime = new Date().toLocaleString('en-US', {
+//     timeZone: 'Europe/Stockholm',
+//     hour: 'numeric',
+//     minute: 'numeric',
+//     second: 'numeric',
+//     hour12: false
+//   });
+
+//   const formattedTime = seTime.replace(/:/g, ' : ');
+//   $se.textContent = formattedTime;
+// }
+
+// function showUsTime() {
+//   const usTime = new Date().toLocaleString('en-US', {
+//     timeZone: 'America/New_York',
+//     hour: 'numeric',
+//     minute: 'numeric',
+//     second: 'numeric',
+//     hour12: false
+//   });
+
+//   const formattedTime = usTime.replace(/:/g, ' : ');
+//   $us.textContent = formattedTime;
+// }
+
+// // 1초마다 showLondonTime 함수 실행
+// setInterval(showLondonTime, 1000);
+// setInterval(showUsTime, 1000);
+// setInterval(showSeTime, 1000);
+
+// ----------------------------------------------------------------------------------
+// FOOTER
+
 
 /*
 git remote add origin https://github.com/ArtisticH/Spotify-Clone.git
