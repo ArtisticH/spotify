6816117ce35a786fbf4fc53a413d2b826a7d7108
서한나 <hanna@seohannaui-MacBookAir.local>
2024-06-10 18:42:34 +0900

    0610

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ app.set('port', process.env.PORT || 8080);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index2.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기 중');
 });
+
