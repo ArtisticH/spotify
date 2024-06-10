90480c5f8bb5b79b17b081f53a4d96ba0ce09535
서한나 <hanna@seohannaui-MacBookAir.local>
2024-06-11 00:07:02 +0900

    0610

--- public/js/app.js
+++ public/js/app.js
@@ -982,7 +982,20 @@ class Footer {
   }
 }
 
+const intro = new Intro();
+new Header();
 const main = new Main();
-
-export { Intro, Header, RelTools, View, Spotlight, Inbox, Footer };
+new RelTools();
+new View();
+const spotlight = new Spotlight();
+const inbox = new Inbox();
+const footer = new Footer();
+document.addEventListener('DOMContentLoaded', intro.init);
+document.body.addEventListener('keydown', spotlight.keydown);
+window.addEventListener('scroll', spotlight.resize);
+window.addEventListener('scroll', inbox.scroll);
+footer.time();
+setInterval(() => {
+  footer.time();
+}, 1000);
 
--- server.js
+++ server.js
@@ -7,8 +7,6 @@ dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 8080);
 
-// app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
