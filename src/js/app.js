68ce6f778b79e31aba259b5db53d84a5ac5356d1
서한나 <hanna@seohannaui-MacBookAir.local>
2024-08-02 21:47:27 +0900

    feat/no-webpack

--- src/js/app.js
+++ src/js/app.js
@@ -983,5 +983,21 @@ class Footer {
   }
 }
 
-export { Intro, Header, Main, RelTools, View, Spotlight, Inbox, Footer };
+const main = new Main();
+const intro = new Intro(main);
+new Header();
+new RelTools();
+new View();
+const spotlight = new Spotlight();
+const inbox = new Inbox();
+const footer = new Footer();
+document.addEventListener('DOMContentLoaded', intro.init);
+document.body.addEventListener('keydown', spotlight.keydown);
+window.addEventListener('scroll', spotlight.resize);
+window.addEventListener('scroll', inbox.scroll);
+footer.time();
+setInterval(() => {
+  footer.time();
+}, 1000);
+
 
